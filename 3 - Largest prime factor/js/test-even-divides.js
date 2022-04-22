479675e47d10865cea73434dae575de8026a46b0
PPSH <info@ppsh.co.uk>
2022-04-23 00:00:29 +0100

    I'm going off on a tangent - commit immediately..!

--- 3 - Largest prime factor/js/test-even-divides.js
+++ 3 - Largest prime factor/js/test-even-divides.js
@@ -13,21 +13,67 @@ Task list:
 */
 
 // Declare variables
-const sampleNumber = 600851475143;
+// const sampleNumber = 600851475143;
+// const sampleNumber = 600851475143;
+const sampleNumber = 60;
 const iterations = 1000000;
 let currentCalc = 0;
 const results = document.getElementById("results");
 
 // Create a loop
 // Let's put this in a function to keep things tidy
-const testRemainder = () => {
-  for (i = 2; i <= iterations; i++) {
-    currentCalc = sampleNumber / i;
-    if (currentCalc % 1 === 0) {
-      results.innerHTML += `${sampleNumber} / ${i} = ${currentCalc}<br />`;
-      // console.log(`${sampleNumber} / ${i} = ${currentCalc}`);
+
+// for (let i = 2; i <= sampleNumber; i++) {
+//   let flag = 0;
+
+//   if (sampleNumber % i === 0) {
+//     flag = 1;
+//     break;
+//   }
+// }
+
+// if (sampleNumber > 1 && flag == 0) {
+//   console.log(sampleNumber);
+// results.innerHTML += `${sampleNumber} / ${j} = ${currentCalc}<br />`;
+// }
+
+function isPrime(num) {
+  console.log(`${num}`);
+  for (let i = 2; i * i <= num; i++)
+    if (num % i === 0) {
+      console.log(`: ${i}`);
+      // return false;
+    }
+  return num > 1;
+}
+
+// console.log(isPrime(13195));
+
+// Idea
+// Create an array of all the prime numbers up to the sample number, then divide the sample number by the array.
+// Might work
+let testNum = 25;
+
+//  for (let i = 2; i < testNum; i++) {
+//    if (i * i === 0)
+//  }
+
+// Brain farts - Let's start again
+function displayAll(num) {
+  results.innerHTML = `<ul class="testing">`;
+  for (let i = 2; i < num; i++) {
+    let flag = 0;
+    for (let j = 2; j < i; j++) {
+      if (i % j == 0) {
+        flag = 1;
+        break;
+      }
+    }
+    if (i > 1 && flag == 0) {
+      results.innerHTML = `<li class="testing">${i}</li>`;
+      console.log(i);
     }
   }
-};
+}
 
-testRemainder();
+displayAll(100);
