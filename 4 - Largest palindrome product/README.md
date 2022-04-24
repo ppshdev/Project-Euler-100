f78a25204eb2e6720ee46ede1928cf66e87e3458
PPSH <info@ppsh.co.uk>
2022-04-24 01:31:03 +0100

    problem 4 - solved

--- 4 - Largest palindrome product/README.md
+++ 4 - Largest palindrome product/README.md
@@ -1,5 +1,7 @@
 # I'm already regretting not starting this when I started.
 
+https://projecteuler.net/problem=4
+
 YOLO - yay...
 
 24/04/2022
@@ -7,3 +9,9 @@ YOLO - yay...
 I think this is working ok. I want to save and commit before I start faffing about and break everything.
 
 Back in a bit.
+
+I'm back, and have worked it out.
+
+Not difficult, and my solution is very messy, but I got there.
+
+In order to get the result you need to open the 'results.json' file and scroll to the bottom of the list. The last entry is the solution.
--- 4 - Largest palindrome product/largest_palindrome.py
+++ 4 - Largest palindrome product/largest_palindrome.py
@@ -3,6 +3,13 @@
 # Find the largest palindrome made from the product of two 3-digit numbers.
 
 # easy bit first - check for palindrome - https://www.w3resource.com/python-exercises/challenges/1/python-challenges-1-exercise-18.php
+
+import json
+
+results = {}
+results_list = []
+
+
 def check_palindrome(num, j, i, result):
     # print(type(result))
     str_num = str(result)
@@ -14,23 +21,41 @@ def check_palindrome(num, j, i, result):
     # print(rev_num)
 
     if result == rev_num:
-        print("palindrome")
-        print(f"{j} * {i} = {result}")
+        results_list.append(result)
+        res_dict = {"j": j, "i": i, "result": result, "string": f"{j} * {i} = {result}"}
+        results[result] = res_dict
+        # print("palindrome")
+        # print(f"{j} * {i} = {result}")
     # else:
     #     print("not palindrome")
 
 
 # check_palindrome(12355321)
-results = []
 
 
 def calculate(num):
 
-    for i in range(10, num):
-        for j in range(10, num):
+    for i in range(100, num):
+        for j in range(100, num):
             result = j * i
             # print(f"{j} * {i} = {result}")
             check_palindrome(num, j, i, result)
 
+    sorted_results = results_list
+    sorted_results.sort()
+    print(sorted_results)
+    with open(
+        "./4 - Largest palindrome product/results.json", "w", encoding="utf-8"
+    ) as f:
+        json.dump(sorted_results, f, indent=4, ensure_ascii=False)
 
-calculate(20)
+
+def save_file():
+    with open(
+        "./4 - Largest palindrome product/palindromes.json", "w", encoding="utf-8"
+    ) as f:
+        json.dump(results, f, indent=4, ensure_ascii=False)
+
+
+calculate(1000)
+save_file()
